6495edc733c94a6b3aab897231929105b346fc9a
kla <dralkoby@live.com>
2019-03-26 20:48:25 -0400

    Finished and save PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,31 +1,46 @@
-print ("You fetch main.py of PyBank")
-
+#print ("You fetch main.py of PyBank") 
 #Date,Profit/Losses
 
 import os
 import csv
 
+def printNwrite(msg):
+    print(msg)
+    writer.write(msg + "\n") 
 
-bank_budget_csv = os.path.join(".", "Resources", "budget_data.csv")
+def createSummary():
+    avgChange = changePrice / (months - 1)
+    printNwrite("Financial Analysis")
+    printNwrite("------------------------------------")
+    printNwrite(f"Total Months: {months}")
+    printNwrite(f"Total: {total}")
+    printNwrite(f"Average Change: {avgChange}")
+    printNwrite(f"Greatest Increase in Profits: {gProfitDate} (${greatProfit})")
+    printNwrite(f"Greatest Decrease in Profits: {lProfitDate} (${greatLoss})")
+
 
 months = 0 
 total = 0
 change = 0
+changeTotal = 0
+greatLoss = 0.00
+greatProfit = 0.00
 prevPrice = 0.00
 changePrice = 0.00
 avgChange = 0.00
 
+bank_budget_csv = os.path.join(".", "Resources", "budget_data.csv")
 
 with open(bank_budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     # Read the header row first (skip this part if there is no header)
     csv_header = next(csvfile)
-    print(f"Header: {csv_header}")
+    # and not to print(f"Header: {csv_header}") 
 
     
      # Read through each row of data after the header
-    for row in csvreader: 
+    for row in csvreader:      
         #Count months
         months += 1
 
@@ -38,14 +53,24 @@ with open(bank_budget_csv, newline="") as csvfile:
             changePrice = changePrice + changeTotal
             prevPrice = float(row[1])
 
-avgChange = changePrice / (months - 1)
-print ("Financial Analysis")
-print ("")
-print ("------------------------------------")
-print(f"Total Months: {months}")
-print(f"Total: {total}")
-print(f"Average Change: {avgChange}")
+        if (greatProfit < changeTotal):
+           greatProfit = changeTotal
+           gProfitDate  = row[0]
+        if (greatLoss> changeTotal) :
+           greatLoss = changeTotal
+           lProfitDate = row[0]
+
+# Set variable and open the output file
+output_file = os.path.join("./Resources","Summary.txt")
+writer = open(output_file,"w",newline="")
+
+
+createSummary()
+
+
+
 
     
-    
 
+
+ 
